b93f4d36bdc2238a72879563951ea543237d3fa0
zlyuanzhang <zlyuanzhang@tencent.com>
2024-05-06 17:19:55 +0800

    完善说明

---
+++ db_table/score_flow_.out.sql
@@ -0,0 +1,52 @@
+create table score_flow_0
+(
+    id           int unsigned auto_increment
+        primary key,
+    oid          varchar(128)     default ''                not null comment '订单id',
+    score_id     int unsigned     default 0                 not null comment '积分类型id',
+    domain       varchar(64)      default ''                not null comment '域',
+
+    o_type       tinyint unsigned default 0                 not null comment '操作类型. 1=增加, 2=扣除, 3=重置',
+    old_score    bigint unsigned  default 0                 not null comment '原始积分',
+    change_score bigint unsigned  default 0                 not null comment '变更积分',
+    o_status     tinyint unsigned default 1                 not null comment '操作状态. 1=成功, 2=余额不足',
+    result_score bigint unsigned  default 0                 not null comment '结果积分',
+
+    uid          varchar(128)     default ''                not null comment '用户唯一标识',
+    remark       varchar(1024)    default ''                not null comment '备注',
+
+    ctime        datetime         default current_timestamp not null,
+    constraint oid_index
+        unique (oid)
+)
+    comment '积分流水';
+
+create index uid_index on score_flow_0 (uid);
+
+
+create table score_flow_1
+(
+    id           int unsigned auto_increment
+        primary key,
+    oid          varchar(128)     default ''                not null comment '订单id',
+    score_id     int unsigned     default 0                 not null comment '积分类型id',
+    domain       varchar(64)      default ''                not null comment '域',
+
+    o_type       tinyint unsigned default 0                 not null comment '操作类型. 1=增加, 2=扣除, 3=重置',
+    old_score    bigint unsigned  default 0                 not null comment '原始积分',
+    change_score bigint unsigned  default 0                 not null comment '变更积分',
+    o_status     tinyint unsigned default 1                 not null comment '操作状态. 1=成功, 2=余额不足',
+    result_score bigint unsigned  default 0                 not null comment '结果积分',
+
+    uid          varchar(128)     default ''                not null comment '用户唯一标识',
+    remark       varchar(1024)    default ''                not null comment '备注',
+
+    ctime        datetime         default current_timestamp not null,
+    constraint oid_index
+        unique (oid)
+)
+    comment '积分流水';
+
+create index uid_index on score_flow_1 (uid);
+
+
---
+++ db_table/score_type.sql
@@ -0,0 +1,15 @@
+create table score_type
+(
+    id                      int unsigned auto_increment comment '积分类型id, 用于区分业务'
+        primary key,
+    score_name              varchar(32)      default ''                                            not null comment '积分名, 与代码无关, 用于告诉配置人员这个积分类型是什么',
+    start_time              datetime                                                               null comment '生效时间',
+    end_time                datetime                                                               null comment '失效时间',
+
+    order_status_expire_day tinyint unsigned default 30                                            not null comment '订单状态保留多少天',
+
+    remark                  varchar(1024)    default ''                                            not null comment '备注',
+    ctime                   datetime         default current_timestamp                             not null comment '创建时间',
+    utime                   datetime         default current_timestamp ON UPDATE CURRENT_TIMESTAMP not null comment '更新时间'
+)
+    comment '积分类型';
